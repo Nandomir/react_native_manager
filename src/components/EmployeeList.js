14312a93fe1ca0e467827cca426e453979f05164
Nandomir <f.sendyka@gmail.com>
2017-06-21 23:16:06 +0100

    Dynamic data source system in EmployeeList

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+
+  componentWillMount() {
+    this.props.employeesFetch();
+
+    // for a listview we require a data source object 
+
+    this.createDataSource(this.props);
+  }
+  
+  componentWillRecieveProps(nextProps) {
+    //nextProps are the next set of props that this component
+    //will be rendered with
+    //this.props is still the old set of props
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
     return (
       <View>
@@ -16,4 +43,4 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
