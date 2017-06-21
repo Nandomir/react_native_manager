6efb69e227b1cb9eebe1e74da6ff85504265fa3d
Nandomir <f.sendyka@gmail.com>
2017-06-21 17:10:53 +0100

    Added employee create scene

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,32 @@
 // This file is responsible for handling all scenes within the app
 
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm.js';
 import EmployeeList from './components/EmployeeList.js';
+import EmployeeCreate from './components/EmployeeCreate.js'
 
 
 const RouterComponent = () => {
   return (
       <Router sceneStyle={{ paddingTop: 65 }}>
-        <Scene key="login" component={LoginForm} title="Please login" initial />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please login" initial />
+        </Scene>
+
+        <Scene key="main">
+          <Scene 
+            onRight={() => Actions.employeeCreate()}
+            rightTitle="Add"
+            key="employeeList" 
+            component={EmployeeList} 
+            title="Employees"
+            initial
+          />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create employee" />
+        </Scene>
+
       </Router>
   );
 };
