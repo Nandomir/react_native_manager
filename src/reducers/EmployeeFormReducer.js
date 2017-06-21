fd6405f0fbf1fb8d9acee16ce95f3b113281f83f
Nandomir <f.sendyka@gmail.com>
2017-06-21 17:42:08 +0100

    index.js in actions now exports all action files

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -2,10 +2,19 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types.js';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane' }
+      // reducer has to return a new object as well
+      return { ...state, [action.payload.prop]: action.payload.value };
+      // the [] above is KEY INTERPOLATION, key added to this object is determined at runtime
     default:
       return state;
   }
