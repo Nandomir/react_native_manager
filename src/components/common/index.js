a80cd46ecf719a4d3b0982bca2987be2b2211445
Nandomir <f.sendyka@gmail.com>
2017-06-22 15:04:38 +0100

    Created modal component Confirm for the purpose of employee deletion confirmation

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -4,6 +4,7 @@ export * from './CardSection.js';
 export * from './Header.js';
 export * from './Input.js';
 export * from './Spinner.js';
+export * from './Confirm.js';
  // in order for this to work, I can't use export 'default'
  // instead exporting an object is required within these files with a key-value pair
  // export { Button: Button } identical pair ES6 allows to - export { Button } 
